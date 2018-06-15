002712919e578e43595d4f2f041c225781f6e063
田中将平 <syouhei@jobby-MBP.local>
2018-06-15 21:34:28 +0900

    Initial commit. Initialize Spring Boot project.

---
+++ src/main/java/com/shotana/serengetibatch/SerengetiBatchApplication.java
@@ -0,0 +1,12 @@
+package com.shotana.serengetibatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerengetiBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerengetiBatchApplication.class, args);
+    }
+}
